50be30d6e2a19b1a77b774bb4155db8ffb732365
TonCD <thichkhampha11@gmail.com>
2023-10-05 22:04:14 +0700

    CRUD - Update users

--- src/service/userService.js
+++ src/service/userService.js
@@ -59,6 +59,36 @@ const deleteUser = async (id) => {
     }
 }
 
+const getUserByID = async (id) =>{
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'jwt',
+        Promise: bluebird
+    });
+    try {
+        const [rows, fields] = await connection.execute('SELECT * FROM users WHERE id = ? ', [id]);
+        return rows;
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+
+const updateUserInfor = async( email, username , id) => {
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'jwt',
+        Promise: bluebird
+    });
+    try {
+        const [rows, fields] = await connection.execute('UPDATE users SET email = ?, username = ? WHERE id = ? ', [email, username, id]);
+        return rows;
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+
 module.exports = {
-    createNewUser, getUserList , deleteUser
+    createNewUser, getUserList , deleteUser, getUserByID, updateUserInfor
 }
